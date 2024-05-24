15d1e4305798ddb09bf1a8dfb4e23b6cb001e7cb
Christian Rasche <crasche@student.codam.nl>
2024-05-24 21:11:11 +0200

    started constructing structures for commands and enum for token types

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/18 16:32:33 by crasche       #+#    #+#                 */
-/*   Updated: 2024/05/18 16:35:02 by crasche       ########   odam.nl         */
+/*   Updated: 2024/05/23 21:36:40 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,33 @@
 # define	MINISHELL_H
 
 # include <stdio.h>
+# include <stdlib.h>
+
+
+
+// PARSING (Chriss)
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef struct s_cmd 
+{
+	char	*cmd;
+	char	**argv;
+	char	*in;
+	char	*out;
+	s_cmd	*next;
+}	t_cmd;
+
+typedef enum e_token_type
+{
+	WORD,
+	PIPE,
+	third
+}	t_token_type;
+// END PARSING
+
+
+
+
 
 #endif	// MINISHELL_H
