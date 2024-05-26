959f23f03e92f284be87e5af6602342be15a8a94
Chr-ss <crasche@student.codam.nl>
2024-05-26 17:45:54 +0200

    Moved readline and parsing parts to extra .c files

--- src/ms_parsing.c
+++ src/ms_parsing.c
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ms_main.c                                          :+:    :+:            */
+/*   ms_parsing.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/05/18 16:29:43 by crasche       #+#    #+#                 */
-/*   Updated: 2024/05/26 17:02:39 by crasche       ########   odam.nl         */
+/*   Created: 2024/05/26 17:02:41 by crasche       #+#    #+#                 */
+/*   Updated: 2024/05/26 17:45:05 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-#include <stdio.h>
-
-void	ms_error(char *msg)
+void	ms_init_cmdlist(t_cmd *cmd)
 {
-	perror(msg);
-	exit(1);
+	cmd = ft_calloc(sizeof(t_cmd), 1);
 }
 
-int	main(int argc, char **argv)
+void	ms_parsing(t_msdata *data)
 {
-	t_msdata	data;
-
-	if (argc > 1)
-		ms_error("Invalid argument count.");
-	(void) argv;
-	ms_parsing(&data);
-	return (0);
+	ms_init_cmdlist(&data->cmd);
 }
--- src/ms_readline.c
+++ src/ms_readline.c
@@ -1,32 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ms_main.c                                          :+:    :+:            */
+/*   ms_readline.c                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/05/18 16:29:43 by crasche       #+#    #+#                 */
-/*   Updated: 2024/05/26 17:02:39 by crasche       ########   odam.nl         */
+/*   Created: 2024/05/26 17:45:15 by crasche       #+#    #+#                 */
+/*   Updated: 2024/05/26 17:45:31 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-#include <stdio.h>
-
-void	ms_error(char *msg)
+char *ms_readline(void)
 {
-	perror(msg);
-	exit(1);
-}
+	char *buffer;
 
-int	main(int argc, char **argv)
-{
-	t_msdata	data;
-
-	if (argc > 1)
-		ms_error("Invalid argument count.");
-	(void) argv;
-	ms_parsing(&data);
-	return (0);
+	while (1)
+	{
+		buffer = readline("minishell:~$");
+		if (!buffer)
+			ms_error("readline malloc error.");
+		printf("%s\n", buffer);
+	}
 }
