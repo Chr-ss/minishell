5c35cd567526751e65bba8ae6d1786d6fd0399f7
Chr-ss <crasche@student.codam.nl>
2024-06-02 17:52:08 +0200

    skipspace utils function for str and pos

--- src/ms_utils_chriss.c
+++ src/ms_utils_chriss.c
@@ -6,7 +6,7 @@
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/01 15:01:31 by crasche       #+#    #+#                 */
-/*   Updated: 2024/06/02 13:02:27 by crasche       ########   odam.nl         */
+/*   Updated: 2024/06/02 16:27:08 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,12 @@ char	*str_expand(char *original_str, int *capacity)
 		free(original_str);
 	}
 	return (new_str);
+}
+
+int	ms_skipspace(char *str, int pos)
+{
+	pos++;
+	while (str && str[pos] && ft_isspace(str[pos]))
+		pos++;
+	return (pos);
 }
