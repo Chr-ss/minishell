d839d47797a3e211301a8e3a2b3e095bc2994fd7
Chr-ss <crasche@student.codam.nl>
2024-06-02 17:49:51 +0200

    split of expension in header files

--- include/expansion.h
+++ include/expansion.h
@@ -1,41 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   lexer.h                                            :+:    :+:            */
+/*   expansion.h                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/24 21:12:18 by crasche       #+#    #+#                 */
-/*   Updated: 2024/06/02 16:32:26 by crasche       ########   odam.nl         */
+/*   Updated: 2024/06/02 17:35:18 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LEXER_H
-# define LEXER_H
+#ifndef EXPANSION_H
+# define EXPANSION_H
 
-typedef enum e_token_type
+typedef struct s_expend
 {
-	TOKEN_WORD,
-	TOKEN_PIPE,
-	TOKEN_REIN,
-	TOKEN_REOUT,
-	TOKEN_APPEND,
-	TOKEN_HEREDOC,
-	TOKEN_EOF,
-	TOKEN_ERROR
-}	t_token_type;
+	char			*env;
+	// int				env_len;
+	char			*line;
+	int				line_pos;
+	int				capacity;
+}	t_expend;
 
-typedef struct s_token
-{
-	char			*start;
-	int				length;
-	t_token_type	type;
-}	t_token;
-
-typedef struct s_tokenizer
-{
-	char	*start;
-	char	*curr;
-}	t_tokenizer;
-
-#endif	// LEXER_H
+#endif	// EXPANSION_H
--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/18 16:32:33 by crasche       #+#    #+#                 */
-/*   Updated: 2024/06/02 13:51:37 by crasche       ########   odam.nl         */
+/*   Updated: 2024/06/02 17:26:52 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@
 
 # include "../libft/include/libft.h"
 # include "lexer.h"
+# include "expansion.h"
 
 # define DYNSTRING 8
+
+# ifndef PATH_MAX
+#  define PATH_MAX 4096
+# endif
 
 typedef struct s_cmd
 {
@@ -39,22 +44,39 @@ typedef struct s_cmd
 
 typedef struct s_msdata
 {
-	t_cmd	*cmd;
-	char	**argv;
-	char	**envp;
+	t_cmd		*cmd;
+	char		*line;
+	char		**argv;
+	char		**envp;
+	char		pwd[PATH_MAX];
+	t_expend	*exp;
 }	t_msdata;
 
 // FUNCTIONS:
+void	ms_readline(t_msdata *data);
+
 void	ms_parsing(t_msdata *data);
-void	ms_init_cmdlist(t_cmd **cmd);
-char	*ms_readline(t_msdata *data, char **argv);
 void	ms_error(char *msg);
 
 // UTILS_CHRISS
 char	*str_expand(char *str, int *capacity);
+int		ms_skipspace(char *str, int pos);
 
-// MS_INITDATA.c
+// INITDATA.c
 void	ms_initdata_cpy_envp(t_msdata *data, char **envp);
 void	ms_initdata(t_msdata *data, char **argv, char **envp);
+
+// PARSING
+void	ms_parsing(t_msdata *data);
+void	ms_parsing_syntax(t_msdata *data);
+int		ms_parsing_syntax_quotes(t_msdata *data);
+
+// EXPANSION
+void	ms_expansion_exp_init(t_msdata *data, t_expend *exp);
+void	ms_expansion_var_nl(t_expend *exp);
+char	*ms_expansion_getenv(char **envp, char *env_start, int length);
+void	ms_expansion_var(t_msdata *data, t_expend *exp, int *pos);
+void	ms_expansion_copy(t_msdata *data, t_expend *exp);
+char	*ms_expansion(t_msdata *data);
 
 #endif	// MINISHELL_H
