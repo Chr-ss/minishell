389be48f24c272fd463026d636908200f953223d
Chr-ss <crasche@student.codam.nl>
2024-06-02 17:52:56 +0200

    ..

--- src/ms_main.c
+++ src/ms_main.c
@@ -6,7 +6,7 @@
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/18 16:29:43 by crasche       #+#    #+#                 */
-/*   Updated: 2024/06/02 13:51:52 by crasche       ########   odam.nl         */
+/*   Updated: 2024/06/02 17:42:09 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,12 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_msdata	data;
-	char		*buffer;
 
 	if (argc > 1)
 		ms_error("Invalid argument count.");
+	printf("%s\n", envp[0]);
 	ms_initdata(&data, argv, envp); // malloc >> data->cmd, data->envp
-	ms_readline(&data, argv);
-	printf("MAIN: %s", buffer);
+	ms_readline(&data);
 	ms_error("No, error.");
 	return (0);
 }
