5f9ce9b55ff574f6ca3ec5e7a912bbcb2ad9e720
spenning <whimizera@mailbox.org>
2024-06-13 20:19:12 +0200

    kvm test

--- tests/e2e/boot_e2e.sh
+++ tests/e2e/boot_e2e.sh
@@ -16,6 +16,8 @@
 # Reference: https://stackoverflow.com/questions/94074/slow-wget-speeds-when-connecting-to-https-pages
 # Reference: https://stackoverflow.com/questions/5613345/how-to-shrink-the-git-folder
 # Reference: https://www.virtualbox.org/manual/ch01.html
+# Reference: https://askubuntu.com/questions/161759/how-to-access-a-shared-folder-in-virtualbox
+# Reference: https://www.jimangel.io/posts/automate-ubuntu-22-04-lts-bare-metal/
 
 XORISSO_EXE=xorriso-1.5.6/xorriso/xorriso
 XORISSO_TAR=xorriso-1.5.6.pl02.tar.gz
@@ -24,28 +26,55 @@ MACHINENAME="TestMachine"
 UBUNTU_CD_IMAGE="ubuntu.iso"
 TEST_CD_IMAGE="ubuntu-22.04.2-test-autoinstall.iso"
 TEST_IMAGE=sources-files
-TEST_IMAGE_USR=server
+TEST_IMAGE_USR=nocloud
 TEST_IMAGE_BTT=bootpart
 USER_DATA=user-data
 
 if ! command -v VBoxManage &> /dev/null
 then
-    echo "VBoxManage could not be found, this is needed to create the VM"
-    exit 1
+	echo "VBoxManage could not be found, this is needed to create the VM"
+	exit 1
 fi
+
+localds=$(find ./ -type f -name "cloud-localds")
+
+
+mkdir -p cidata
+cat > cidata/user-data << 'EOF'
+#cloud-config
+autoinstall:
+  version: 1
+  identity:
+    hostname: ubuntu-server
+    password: "$6$exDY1mhS4KUYCE/2$zmn9ToZwTKLhCw.b4/b.ZRTIZM30JZ4QrOQ2aOXJ8yk96xpcCof0kxKwuX1kqLG/ygbJ1f8wxED22bTL4F46P0"
+    username: ubuntu
+EOF
+echo > cidata/meta-data
+
+./$localds seed.iso cidata/user-data cidata/meta-data
+
+echo $(pwd)
+
+kvm -no-reboot -m 2048 \
+    -drive file=image.img,format=raw,cache=none,if=virtio \
+    -drive file=seed.iso,format=raw,cache=none,if=virtio \
+    -cdrom $UBUNTU_CD_IMAGE
+
+exit 0
+
 
 # Download xorriso
 if [ ! -f ./$XORISSO_EXE ]; then
-    echo -ne 'Downloading xorriso       : #                         (0%)\r'
-    wget https://www.gnu.org/software/xorriso/xorriso-1.5.6.pl02.tar.gz -O $XORISSO_TAR &> /dev/null
-    echo -ne 'Unpacking xorriso         : #####                     (33%)\r'
-    tar zxvf $XORISSO_TAR &> /dev/null
-    echo -ne 'Building xorriso          : #############             (66%)\r'
-    (cd $XORISSO_DIR && ./configure && make) &> /dev/null
-    echo -ne 'installing xorisso        : ######################    (95%)\r' 
-    rm -rf $XORISSO_TAR &> /dev/null
-    echo -ne 'xorisso installed         : #######################   (100%)\r' 
-    echo -ne '\n'
+	echo -ne 'Downloading xorriso       : #                         (0%)\r'
+	wget https://www.gnu.org/software/xorriso/xorriso-1.5.6.pl02.tar.gz -O $XORISSO_TAR &> /dev/null
+	echo -ne 'Unpacking xorriso         : #####                     (33%)\r'
+	tar zxvf $XORISSO_TAR &> /dev/null
+	echo -ne 'Building xorriso          : #############             (66%)\r'
+	(cd $XORISSO_DIR && ./configure && make) &> /dev/null
+	echo -ne 'installing xorisso        : ######################    (95%)\r' 
+	rm -rf $XORISSO_TAR &> /dev/null
+	echo -ne 'xorisso installed         : #######################   (100%)\r' 
+	echo -ne '\n'
 fi 
 
 # Add xorriso to path 
@@ -55,26 +84,38 @@ export PATH=$PATH:$XORISSO_PATH
 fi 
 
 # Download debian.iso
-# if [ ! -f ./$UBUNTU_CD_IMAGE ]; then
-#     echo -ne 'Downloading ubuntu image  : #                         (0%) (this might take a few minutes)\r'
-#     wget https://releases.ubuntu.com/jammy/ubuntu-22.04.4-live-server-amd64.iso --inet4-only --no-check-certificate -O $UBUNTU_CD_IMAGE 
-#     echo -ne 'Downloading complete      : #######################   (100%)\r'
-# fi
+if [ ! -f ./$UBUNTU_CD_IMAGE ]; then
+	echo -ne 'Downloading ubuntu image  : #                         (0%) (this might take a few minutes)\r'
+	wget https://releases.ubuntu.com/jammy/ubuntu-22.04.4-live-server-amd64.iso --inet4-only --no-check-certificate -O $UBUNTU_CD_IMAGE 
+	echo -ne 'Downloading complete      : #######################   (100%)\r'
+fi
 
 # Extract iso image 
 if [ ! -d ./$TEST_IMAGE ]; then
-    mkdir -p $TEST_IMAGE/$TEST_IMAGE_BTT
-    xorriso -osirrox on -indev $UBUNTU_CD_IMAGE --extract_boot_images $TEST_IMAGE/$TEST_IMAGE_BTT -extract / $TEST_IMAGE 
-    chmod -R u+w $TEST_IMAGE
+	mkdir -p $TEST_IMAGE/$TEST_IMAGE_BTT
+	xorriso -osirrox on -indev $UBUNTU_CD_IMAGE --extract_boot_images $TEST_IMAGE/$TEST_IMAGE_BTT -extract / $TEST_IMAGE 
+	chmod -R u+w $TEST_IMAGE
 fi
 
 #create folder + user-data / meta-data for autoinstall
-(cd $TEST_IMAGE && mkdir -p $TEST_IMAGE_USR && cd $TEST_IMAGE_USR && cp $(find ../../../ -type d -name util)/$USER_DATA ./ && echo > meta-data)
+(cd $TEST_IMAGE && mkdir -p $TEST_IMAGE_USR && cd $TEST_IMAGE_USR && echo > meta-data)
 
 # find files
 GRUB_FILE=$(find ./ -type f -name grub.cfg)
 GRUB2_MBR=$(find ./sources-files -type f -name "mbr_code_grub2.img")
 EFI=$(find ./ -type f -name "gpt_part2_efi.img")
+
+echo $USER_DATA_DIR
+
+cat << 'EOF' > $TEST_IMAGE/$TEST_IMAGE_USR/user-data
+#cloud-config
+autoinstall:
+version: 1
+identity:
+    hostname: ubuntu-server
+    password: "$6$exDY1mhS4KUYCE/2$zmn9ToZwTKLhCw.b4/b.ZRTIZM30JZ4QrOQ2aOXJ8yk96xpcCof0kxKwuX1kqLG/ygbJ1f8wxED22bTL4F46P0"
+    username: ubuntu
+EOF
 
 #change grub boot
 cat << "EOF" > $(find ./ -type f -name grub.cfg)
@@ -86,9 +127,9 @@ set menu_color_normal=white/black
 set menu_color_highlight=black/light-gray
 
 menuentry "Autoinstall Ubuntu Server" {
-set gfxpayload=keep
-linux /casper/vmlinuz quiet autoinstall ds=nocloud\;s=/cdrom/server/ ---
-initrd /casper/initrd
+	set gfxpayload=keep
+	linux /casper/vmlinuz quiet autoinstall ds=nocloud\;s=/cdrom/nocloud/ ---
+	initrd /casper/initrd
 }
 menuentry "Try or Install Ubuntu Server" {
 	set gfxpayload=keep
@@ -115,11 +156,14 @@ menuentry 'Test memory' {
 fi
 EOF
 
+
 # remove ubuntu image for diskspace
-rm -rf $UBUNTU_CD_IMAGE
+# rm -rf $UBUNTU_CD_IMAGE
+
 
 # Make new iso file for test vm
 if [ ! -f ./$TEST_CD_IMAGE ]; then
+echo -ne 'Creating test machine image  : #                         (0%) \r'
 (cd $TEST_IMAGE && cat <<EOF | bash)
 xorriso -as mkisofs -r \
 -V 'Ubuntu 22.04.2 LTS amd64' \
@@ -139,29 +183,49 @@ xorriso -as mkisofs -r \
 -e '--interval:appended_partition_2_start_971297s_size_10068d:all::' \
 -no-emul-boot .
 EOF
+echo -ne 'Creating test machine image  : #######################   (100%)\r'
 fi
 
-rm -rf $TEST_IMAGE
+#rm -rf $TEST_IMAGE
 
-#Create VM
-VBoxManage createvm --name $MACHINENAME --ostype "Ubuntu22_LTS_64" --register --basefolder `pwd`
-#Set memory and network
-VBoxManage modifyvm $MACHINENAME --ioapic on
-VBoxManage modifyvm $MACHINENAME --memory 2048 --vram 128
-VBoxManage modifyvm $MACHINENAME --nic1 nat
-#Create Disk and connect Debian Iso
-VBoxManage createhd --filename `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi --size 3000 --format VDI
-VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
-VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi
-VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
-VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium `pwd`/$TEST_CD_IMAGE
-VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
+truncate -s 3G image.img
 
-#Enable RDP
-VBoxManage modifyvm $MACHINENAME --vrde on
-VBoxManage modifyvm $MACHINENAME --vrdemulticon on --vrdeport 10001
+# kvm -no-reboot -m 2048 \
+#     -drive file=image.img,format=raw,cache=none,if=virtio \
+# 	-cdrom $TEST_CD_IMAGE
+#     # -drive file=$UBUNTU_CD_IMAGE,format=raw,cache=none,if=virtio \
 
-#Start the VM
-VBoxManage startvm $MACHINENAME
 
-# VBoxHeadless --startvm $MACHINENAME
+
+
+kvm -no-reboot -m 2048 \
+    -drive file=image.img,format=raw,cache=none,if=virtio \
+    -cdrom $TEST_CD_IMAGE \
+    -kernel $TEST_IMAGE/casper/vmlinuz \
+    -initrd $TEST_IMAGE/casper/initrd \
+    -append 'root=/dev/ram0 ramdisk_size=1500000 ip=dhcp autoinstall ds=nocloud\;s=/cdrom/nocloud/ ---'
+
+
+# #Create VM
+# VBoxManage createvm --name $MACHINENAME --ostype "Ubuntu22_LTS_64" --register --basefolder `pwd`
+# #Set memory and network
+# VBoxManage modifyvm $MACHINENAME --ioapic on
+# VBoxManage modifyvm $MACHINENAME --memory 3062 
+# VBoxManage modifyvm $MACHINENAME --nic1 nat
+# #Create Disk and connect Debian Iso
+# VBoxManage createhd --filename `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi --size 4000 --format VDI
+# VBoxManage storagectl $MACHINENAME --name "SATA Controller" --add sata --controller IntelAhci
+# VBoxManage storageattach $MACHINENAME --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium  `pwd`/$MACHINENAME/$MACHINENAME_DISK.vdi
+# VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
+# VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium `pwd`/$TEST_CD_IMAGE
+# VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
+
+# #Enable RDP
+# VBoxManage modifyvm $MACHINENAME --vrde on
+# VBoxManage modifyvm $MACHINENAME --vrdemulticon on --vrdeport 10001
+
+# #Start the VM
+# VBoxManage startvm $MACHINENAME
+# # VBoxHeadless --startvm $MACHINENAME
+
+# rm -rf $TEST_CD_IMAGE
