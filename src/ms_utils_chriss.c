376170f8ddfe77975ff94723edc2581422da773a
Chr-ss <crasche@student.codam.nl>
2024-06-23 14:30:56 +0200

    moved  dyn array functions to utils files

--- src/ms_utils_chriss.c
+++ src/ms_utils_chriss.c
@@ -6,7 +6,7 @@
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/01 15:01:31 by crasche       #+#    #+#                 */
-/*   Updated: 2024/06/20 17:31:04 by crasche       ########   odam.nl         */
+/*   Updated: 2024/06/23 14:28:21 by crasche       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,35 @@ int	ms_skipspace(char *str, int pos)
 	while (str && str[pos] && ft_isspace(str[pos]))
 		pos++;
 	return (pos);
+}
+
+int	ms_strarr_size(char **strarr)
+{
+	int	i;
+
+	i = 0;
+	while (strarr && strarr[i])
+		i++;
+	return (i);
+}
+
+char	**ms_extend_strarr(t_cmd *cmd, char **strarr, int strarr_size)
+{
+	(void) cmd;
+	int		i;
+	char	**new_strarr;
+
+	i = 0;
+	strarr_size++;
+	new_strarr = calloc(strarr_size + 1, sizeof(char*));
+	if (!new_strarr)
+		ms_error("ms_extend_strarr: malloc error.");
+	while (strarr && strarr[i])
+	{
+		new_strarr[i] = strarr[i];
+		strarr[i] = NULL;
+		i++;
+	}
+	free(strarr);
+	return (new_strarr);
 }
