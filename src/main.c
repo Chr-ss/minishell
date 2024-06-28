8958516ef8d6a8ce1b654cd1e2afc9754755d230
Christian <christian.rasche@bertelsmann.de>
2024-06-28 17:37:18 +0200

    Added check_tty to main without calling it yet

--- src/main.c
+++ src/main.c
@@ -12,6 +12,26 @@
 
 #include "../include/minishell.h"
 
+// This function still needs to be called at some point to check if our stdin and out are tty!!
+void	check_tty(void)
+{
+	if (!isatty(STDIN_FILENO))
+	{
+		write (2, "minishell: stdin: not a tty\n", 28);
+		exit (1);
+	}
+	if (!isatty(STDOUT_FILENO))
+	{
+		write (2, "minishell: stdout: not a tty\n", 29);
+		exit (1);
+	}
+	if (!isatty(STDERR_FILENO))
+	{
+		write (2, "minishell: stderr: not a tty\n", 29);
+		exit (1);
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_msdata	data;
