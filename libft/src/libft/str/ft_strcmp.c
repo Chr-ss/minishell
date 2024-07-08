7f2882465f20656f905b0ea8a68d6ad711535921
Chr-ss <crasche@student.codam.nl>
2024-07-08 17:41:16 +0200

    added strcmp in libft

---
+++ libft/src/libft/str/ft_strcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strcmp.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: crasche <crasche@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/03 15:31:31 by crasche       #+#    #+#                 */
+/*   Updated: 2024/07/08 14:54:20 by crasche       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	return (0);
+}
