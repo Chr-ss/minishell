730a8d94ff14aacc90ab7144f259171188b1f49a
sebastianpenning <whimizera@mailbox.org>
2024-07-09 19:47:46 +0200

    added bash script to run docker tests

---
+++ run_docker.sh
@@ -0,0 +1,20 @@
+#https://stackoverflow.com/questions/70852648/remove-docker-image-if-it-exists
+
+
+check_image=$(docker images -af reference='minishell/tester' -q)
+check_container=$(docker ps -a | grep 'minishell/tester' | awk '{print $1}')
+
+
+if [ -z "$check_container" ];
+then :
+else docker rm $check_container
+fi
+
+if [ -z "$check_image" ];
+then :
+else docker rmi $check_image
+fi
+
+docker build -t minishell/tester .
+
+docker run minishell/tester
