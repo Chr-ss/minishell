f80c3387fa5998f0df106146359874e6e2cfbc6c
spenning <whimizera@mailbox.org>
2024-07-09 10:33:57 +0200

    fix no argv segfault

--- src/utils/etc.c
+++ src/utils/etc.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   etc.c                                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/24 18:44:04 by spenning          #+#    #+#             */
-/*   Updated: 2024/06/24 18:53:14 by spenning         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   etc.c                                              :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spenning <spenning@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/06/24 18:44:04 by spenning      #+#    #+#                 */
+/*   Updated: 2024/07/09 10:27:50 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	double_array_len(char **vector)
 {
 	int	index;
 
+	if (vector == NULL)
+		return (0);
 	index = 0;
 	while (vector[index] != NULL)
 		index++;
