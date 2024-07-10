54b7959f37f4a65d782a943cda4934f0b8ceaa3c
spenning <whimizera@mailbox.org>
2024-07-10 13:14:50 +0200

    fix with debugger

--- src/utils/debug.c
+++ src/utils/debug.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/27 15:02:08 by spenning      #+#    #+#                 */
-/*   Updated: 2024/07/09 17:58:19 by crasche       ########   odam.nl         */
+/*   Updated: 2024/07/10 13:11:31 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ void	debugger(char *format, ...)
 {
 	va_list	ptr;
 
+
 	if (DEBUG == 1)
 	{
 		va_start(ptr, format);
-		ft_printf(format, ptr);
+		vprintf(format, ptr);
 		va_end(ptr);
 	}
 }
