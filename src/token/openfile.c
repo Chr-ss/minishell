009ae3cf5ef0e3dce0cdc0842e17d75adfddd91a
spenning <whimizera@mailbox.org>
2024-07-24 17:57:00 +0200

    openfile conditions restored and corrected

--- src/token/openfile.c
+++ src/token/openfile.c
@@ -6,7 +6,7 @@
 /*   By: crasche <crasche@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/18 16:29:43 by crasche       #+#    #+#                 */
-/*   Updated: 2024/07/24 09:42:22 by mynodeus      ########   odam.nl         */
+/*   Updated: 2024/07/24 17:53:19 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,14 @@ void	openfile(t_msdata *data, t_token token, int open_flag, int *fd)
 	if (!filename)
 		error("openfile: malloc error.", data);
 	new_fd = open(filename, open_flag, 0644);
+	if (*fd > 0)
+	{
+		close(*fd);
+		*fd = 0;
+	}
 	if (*fd == 0)
 		*fd = new_fd;
+	if (*fd < 0 && new_fd > 0)
+		close (new_fd);
 	free(filename);
 }
